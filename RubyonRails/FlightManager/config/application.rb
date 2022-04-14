c723711b9935c86bef4fe91f7618575b1f604fae
pulgamecanica <pulgamecanica11@gmail.com>
2022-04-14 11:48:02 +0100

    Ruby on Rails -delete

--- RubyonRails/FlightManager/config/application.rb
+++
@@ -1,9 +0,0 @@
-require 'active_record'
-require 'sqlite3'
-require 'yaml'
-require_relative '../app/models/model'
-require_relative '../app/controllers/controller'
-require_relative '../app/views/view'
-
-configuration = YAML::load_file(File.join(__dir__, 'database.yml'))
-ActiveRecord::Base.establish_connection(configuration['development'])
--- RubyonRails/RubyonRails_02/config/routes.rb
+++
@@ -1,8 +0,0 @@
-Rails.application.routes.draw do
-  
- root 'words#home'
-
- post '/anagrams' => 'words#new_word'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
